b0449f8fb9073054b0267d17192430a33fd1af2f
Sky <sky@thelook.cn>
2020-08-10 12:26:10 +0800

    refactor: standalone routers

--- main.go
+++ main.go
@@ -6,16 +6,12 @@ import (
 
 	"github.com/gin-gonic/gin"
 
+	"gin-example/routers"
 	"gin-example/util"
 )
 
 func main() {
-	router := gin.Default()
-	router.GET("/test", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "test",
-		})
-	})
+	router := routers.InitRouter()
 
 	s := &http.Server{
 		Addr:           fmt.Sprintf(":%d", util.HTTPPort),
